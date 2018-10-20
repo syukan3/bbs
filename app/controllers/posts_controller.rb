4136c97e40a7cf3715bf458233b31dfd34c46c8c
syukan3 <en421430@s.okayama-u.ac.jp>
2018-10-20 15:31:59 +0900

    Add room2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -49,6 +49,7 @@ class PostsController < ApplicationController
 
   def destroy
     @post = Post.find_by(id: params[:id])
+    @post.destroy
     flash[:notice] = "投稿を削除しました。"
     redirect_to(posts_path)
   end
